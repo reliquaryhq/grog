58be1a91d6f96bc52ea54bcf048d4b6c2cb538e8
fallenoak <git@fallenoak.me>
2019-10-26 14:40:52 -0500

    chore: expose additional dirs in fs util

--- src/util/fs.mjs
+++ src/util/fs.mjs
@@ -1,5 +1,11 @@
+import path from 'path';
 import MODULE_DIR from './moduleDir.js';
+
+const SRC_DIR = path.resolve(MODULE_DIR, 'src');
+const SCHEMA_DIR = path.resolve(SRC_DIR, 'db/schema');
 
 export {
   MODULE_DIR,
+  SCHEMA_DIR,
+  SRC_DIR,
 };
