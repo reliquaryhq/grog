d8297b8a9caa97d9b457057311f9f46af151fb5a
fallenoak <git@fallenoak.me>
2019-11-05 21:35:45 -0600

    chore: add string utils

---
+++ src/util/string.mjs
@@ -0,0 +1,31 @@
+const formatBytes = (bytes, decimals = 2) => {
+  if (bytes === 0) {
+    return '0 Bytes';
+  }
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  const bytesString = parseFloat((bytes / Math.pow(k, i)).toFixed(dm)).toString();
+  return `${bytesString}${bytesString.includes('.') ? '' : '.0'} ${sizes[i]}`;
+};
+
+const formatFixedWidthString = (string, width, align = 'left') => {
+  if (string.length <= width) {
+    return align === 'left'
+      ? string.padEnd(width, ' ')
+      : string.padStart(width, ' ');
+  }
+
+  return align === 'left'
+    ? `${string.substring(0, width - 3)}...`
+    : `...${string.substring(string.length - width + 3)}`;
+};
+
+export {
+  formatBytes,
+  formatFixedWidthString,
+};
