86befdce3d845dfb348b0df1146ad6507c5a306b
fallenoak <git@fallenoak.me>
2019-11-06 22:44:25 -0600

    chore: add initial download queue implementation

---
+++ src/util/DownloadQueue.mjs
@@ -0,0 +1,78 @@
+import https from 'https';
+import Progress from 'progress';
+import { formatBytes, formatFixedWidthString } from './string.mjs';
+
+class DownloadQueue {
+  constructor(host, downloadRoot, handleDownload) {
+    this.host = host;
+    this.downloadRoot = downloadRoot;
+    this.handleDownload = handleDownload;
+    this.entries = {};
+    this.totalSize = 0;
+  }
+
+  add(entry) {
+    this.entries[entry.path] = entry;
+
+    if (entry.size) {
+      this.totalSize += entry.size;
+    }
+  }
+
+  async run() {
+    const entries = Object.values(this.entries);
+    const agent = new https.Agent({ keepAlive: true });
+    let downloadedSize = 0;
+
+    const progress = new Progress(
+      'Downloading  :name  [:bar]  :etas  :current/:total entries  :downloadedSize/:totalSize',
+      {
+        width: 70,
+        total: entries.length,
+        downloadedSize: formatBytes(downloadedSize),
+        totalSize: this.totalSize > 0 ? formatBytes(this.totalSize) : '?',
+        name: formatFixedWidthString('', 50, 'right'),
+      }
+    );
+
+    const onProgress = (entry, bytes) => {
+      downloadedSize += bytes;
+
+      progress.tick(0, {
+        downloadedSize: formatBytes(downloadedSize),
+        totalSize: this.totalSize > 0 ? formatBytes(this.totalSize) : '?',
+        name: formatFixedWidthString(entry.path, 50, 'right'),
+      });
+    };
+
+    for (const entry of entries) {
+      progress.tick(0, {
+        downloadedSize: formatBytes(downloadedSize),
+        totalSize: this.totalSize > 0 ? formatBytes(this.totalSize) : '?',
+        name: formatFixedWidthString(entry.path, 50, 'right'),
+      });
+
+      const onHeaders = (headers) => {
+        if (!entry.size) {
+          this.totalSize += parseInt(headers['content-length'], 10);
+        }
+      };
+
+      await this.handleDownload(
+        { ...entry, host: this.host },
+        this.downloadRoot,
+        agent,
+        onHeaders,
+        onProgress
+      );
+
+      progress.tick(1, {
+        downloadedSize: formatBytes(downloadedSize),
+        totalSize: this.totalSize > 0 ? formatBytes(this.totalSize) : '?',
+        name: formatFixedWidthString(entry.path, 50, 'right'),
+      });
+    }
+  }
+}
+
+export default DownloadQueue;
