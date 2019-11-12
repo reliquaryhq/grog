9dba8fdb3bb415179dc9d70c6f72be85a3eee5cf
fallenoak <git@fallenoak.me>
2019-11-11 23:08:01 -0600

    chore: add retry util

--- src/util/common.mjs
+++ src/util/common.mjs
@@ -24,6 +24,26 @@ const hashObject = (object) => crypto.createHash('sha256')
   .update(JSON.stringify(object))
   .digest('hex');
 
+const retry = async (maxAttempts, delay, func, attempt = 0, error = null) => {
+  if (attempt >= maxAttempts) {
+    if (error) {
+      throw error;
+    }
+
+    return;
+  }
+
+  try {
+    return await func(attempt, error);
+  } catch (error) {
+    if (delay !== 0) {
+      await sleep(delay);
+    }
+
+    return await retry(maxAttempts, delay, func, attempt + 1, error);
+  }
+};
+
 const sleep = (milliseconds) =>
   new Promise(resolve => setTimeout(resolve, milliseconds));
 
@@ -37,6 +57,7 @@ export {
   ensureDb,
   ensureInit,
   hashObject,
+  retry,
   sleep,
   sortAsNumbers,
   sortObject,
