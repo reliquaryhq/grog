2c43c369a22c06c8d5a3294c377ed56e0b629444
fallenoak <git@fallenoak.me>
2019-11-11 23:09:47 -0600

    feat: add all flag to mirror product cli command

--- src/cli/mirror.mjs
+++ src/cli/mirror.mjs
@@ -1,9 +1,25 @@
-import { ensureDb } from '../util/common.mjs';
+import { ensureDb, sleep } from '../util/common.mjs';
 import { mirrorProduct } from '../util/mirror.mjs';
+import * as api from '../api.mjs';
 
 const handleMirrorProduct = async (_args, flags) => {
-  const productId = flags['product-id'];
-  await mirrorProduct(productId);
+  if (flags['all']) {
+    const { totalPages } = await api.product.getCatalogProducts(1, 'release_desc');
+
+    for (let page = 1; page <= totalPages; page++) {
+      console.log(`Mirroring catalog products from page ${page} of ${totalPages}...`);
+
+      const { products = [] } = await api.product.getCatalogProducts(page, 'release_desc');
+
+      for (const product of products) {
+        await mirrorProduct(product.id);
+        await sleep(2000);
+      }
+    }
+  } else {
+    const productId = flags['product-id'];
+    await mirrorProduct(productId);
+  }
 };
 
 const handleMirror = async ([command, ...args], flags) => {
