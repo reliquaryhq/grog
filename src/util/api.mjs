c4b34254d89f3f73deef8fab32e587c31b7b71f1
fallenoak <git@fallenoak.me>
2019-11-11 22:35:21 -0600

    chore: add GOG_WWW_HEADERS and GOG_WWW_URL

--- src/util/api.mjs
+++ src/util/api.mjs
@@ -1,15 +1,20 @@
 const GOG_GALAXY_USER_AGENT = 'GOGGalaxyClient/1.2.59.21 (win10 x64)';
+const CHROME_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36';
 
 const GOG_API_URL = 'https://api.gog.com';
 const GOG_CDN_URL = 'https://cdn.gog.com';
 const GOG_CS_URL = 'https://content-system.gog.com';
 const GOG_IMAGES_URL = 'https://images.gog.com';
+const GOG_WWW_URL = 'https://www.gog.com';
 
 const GOG_API_HEADERS = {
   'User-Agent': GOG_GALAXY_USER_AGENT,
 };
 const GOG_CS_HEADERS = {
   'User-Agent': GOG_GALAXY_USER_AGENT,
+};
+const GOG_WWW_HEADERS = {
+  'User-Agent': CHROME_USER_AGENT,
 };
 
 export {
@@ -19,4 +24,6 @@ export {
   GOG_CS_HEADERS,
   GOG_CS_URL,
   GOG_IMAGES_URL,
+  GOG_WWW_HEADERS,
+  GOG_WWW_URL,
 };
