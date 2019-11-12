b1496b1934e321973da654ee26626bf7619816f9
fallenoak <git@fallenoak.me>
2019-11-11 22:49:00 -0600

    chore: add getCatalogProducts api helper

--- src/api/product.mjs
+++ src/api/product.mjs
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
-import { GOG_API_HEADERS, GOG_API_URL } from '../util/api.mjs';
-import { getJson, handleError } from '../util/http.mjs';
+import { GOG_API_HEADERS, GOG_API_URL, GOG_WWW_HEADERS, GOG_WWW_URL } from '../util/api.mjs';
+import { getJson, handleError, getQuery } from '../util/http.mjs';
 
 const PRODUCT_EXPAND = [
   'downloads',
@@ -12,6 +12,14 @@ const PRODUCT_EXPAND = [
   'changelog',
 ];
 
+const getCatalogProducts = (page, sort, mediaType = null) =>
+  fetch(
+    `${GOG_WWW_URL}/games/ajax/filtered?${getQuery({ mediaType, page, sort })}`,
+    { headers: GOG_WWW_HEADERS },
+  )
+  .then(handleError)
+  .then(getJson);
+
 const getProduct = (productId) =>
   fetch(
     `${GOG_API_URL}/products/${productId}?expand=${PRODUCT_EXPAND.join(',')}`,
@@ -21,5 +29,6 @@ const getProduct = (productId) =>
   .then(getJson);
 
 export {
+  getCatalogProducts,
   getProduct,
 };
