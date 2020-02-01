86d6fae28bcf91b4dfc0cbbc0443d9ba9adbf8fd
fallenoak <git@fallenoak.me>
2020-02-01 13:17:43 -0600

    chore: add schema for downlinks

---
+++ src/db/schema/0005.mjs
@@ -0,0 +1,35 @@
+const up = async (connection, sql) => {
+  return connection.query(sql`
+    INSERT INTO asset_types
+      (name, slug)
+    VALUES
+      ('Bonus Content File', 'bonus-content-file'),
+      ('Installer File', 'installer-file'),
+      ('Language Pack File', 'language-pack-file'),
+      ('Patch File', 'patch-file');
+
+    CREATE TABLE downlinks (
+      id BIGSERIAL,
+      product_id BIGINT NOT NULL,
+      asset_id BIGINT,
+      file_id TEXT NOT NULL,
+      downlink_type TEXT NOT NULL,
+      downlink_path TEXT NOT NULL,
+      size BIGINT
+    );
+  `);
+};
+
+const down = async (connection, sql) => {
+  return connection.query(sql`
+    DELETE FROM asset_types
+      WHERE slug = 'bonus-content-file'
+      OR slug = 'installer-file'
+      OR slug = 'language-pack-file'
+      OR slug = 'patch-file';
+
+    DROP TABLE downlinks;
+  `);
+}
+
+export { up, down };
