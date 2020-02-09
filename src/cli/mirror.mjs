1c063f3db89e35ad2c941410c46869a49b57f0f4
fallenoak <git@fallenoak.me>
2020-02-09 00:58:40 -0600

    feat: track owned products and support page flag when mirroring

--- src/cli/mirror.mjs
+++ src/cli/mirror.mjs
@@ -1,24 +1,40 @@
 import { ensureDb, sleep } from '../util/common.mjs';
 import { mirrorProduct } from '../util/mirror.mjs';
+import { loadSession, saveSession } from '../util/session.mjs';
 import * as api from '../api.mjs';
 
 const handleMirrorProduct = async (_args, flags) => {
+  const session = await loadSession();
+
+  let ownedGames = [];
+
+  if (session) {
+    console.log('Fetching list of owned games...');
+
+    const userGames = await api.embed.getUserGames(await session.getBearer());
+    ownedGames = userGames.owned;
+
+    await saveSession(session);
+  }
+
   if (flags['all']) {
     const { totalPages } = await api.product.getCatalogProducts(1, 'release_desc');
+    const firstPage = flags['page'] ? parseInt(flags['page'], 10) : 1;
 
-    for (let page = 1; page <= totalPages; page++) {
+    for (let page = firstPage; page <= totalPages; page++) {
       console.log(`Mirroring catalog products from page ${page} of ${totalPages}...`);
 
       const { products = [] } = await api.product.getCatalogProducts(page, 'release_desc');
 
       for (const product of products) {
-        await mirrorProduct(product.id);
+        await mirrorProduct(product.id, ownedGames);
         await sleep(1000);
       }
     }
   } else {
-    const productId = flags['product-id'];
-    await mirrorProduct(productId);
+    const productId = parseInt(flags['product-id'], 10);
+
+    await mirrorProduct(productId, ownedGames);
   }
 };
 
