7ae7065da95eaebf8c118d91673321812c75448b
fallenoak <git@fallenoak.me>
2020-03-07 23:07:44 -0600

    chore: use better name for import

--- src/util/process.mjs
+++ src/util/process.mjs
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
-import process from 'child_process';
+import childProcess from 'child_process';
 
 const config = dotenv.config();
 const env = config.parsed ? config.parsed : {};
 
 const exec = (command, options = {}) => {
   return new Promise((resolve, reject) => {
-    process.exec(command, options, (error, stdout, stderr) => {
+    childProcess.exec(command, options, (error, stdout, stderr) => {
       if (error) {
         reject(error);
       } else {
