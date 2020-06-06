c371fa3b9e42e1397db2442af862060728318a0b
fallenoak <git@fallenoak.me>
2020-06-06 12:50:04 -0500

    feat: improve read product builds command

--- src/cli/read.mjs
+++ src/cli/read.mjs
@@ -28,24 +28,41 @@ const handleReadProductBuilds = async (_args, flags) => {
   const productId = flags['product-id'];
   const os = flags['os'];
 
-  const productBuilds = await db.product.getApiProductBuilds({ productId, os });
+  const productBuilds = await db.product.getAllApiProductBuilds({ productId });
 
   if (!productBuilds) {
     console.log('Product builds not found');
     return;
   }
 
-  if (flags['only-data']) {
-    console.log(JSON.stringify(productBuilds['data'], null, 2));
+  const mergedProductBuilds = {};
+
+  for (const { data } of productBuilds) {
+    for (const item of data.items || []) {
+      if (os && item.os !== os) {
+        continue;
+      }
+
+      const manifestUrl = item.urls[0].url;
+      const manifestPath = `/${manifestUrl.split('/').slice(3).join('/')}`;
+
+      mergedProductBuilds[item.build_id] = {
+        build_id: item.build_id,
+        generation: item.generation,
+        version_name: item.version_name,
+        product_id: item.product_id,
+        os: item.os,
+        manifest_path: manifestPath,
+      };
+    }
+  }
+
+  if (flags['as-json']) {
+    console.log(JSON.stringify(Object.values(mergedProductBuilds), null, 2));
     return;
   }
 
-  console.log(JSON.stringify({
-    'id': productBuilds['product_id'],
-    'revision': productBuilds['revision'],
-    'revision_first_seen_at': new Date(productBuilds['revision_first_seen_at']).toISOString(),
-    'revision_last_seen_at': new Date(productBuilds['revision_first_seen_at']).toISOString(),
-  }, null, 2));
+  console.log(Object.values(mergedProductBuilds));
 };
 
 const handleRead = async ([command, ...args], flags) => {
